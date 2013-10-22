c1eb32bd588a6f17ec8d2d3a5be4c4d6b64368e1
Jonas Almeida <jalmeida@mathbiol.org>
2013-10-22 17:41:05 -0500

    VCF module

--- manifest.js
+++ manifest.js
@@ -32,6 +32,21 @@ wApps.manifest.tabs={
 
 // 3) APPS - the description of the applications 
 wApps.manifest.apps.push(
+
+    {
+    "name" : 'VCF',
+    "description" : 'web app to handle <a href="http://vcftools.sourceforge.net/specs.html" target="_blank">Variant Calling Format</a> genomic data.',
+    "url" : 'https://github.com/ibl/vcf',
+    "author" : 'Jonas Almeida',
+    buildUI : function(id){ 
+        this.require('https://raw.github.com/ibl/vcf/gh-pages/vcf.js', 
+            function () {
+                VCF.buildUI(id);
+            }
+        )}
+    },
+
+
     {
     "name": "QMachine",
     "description": "QMachine volunteer",
